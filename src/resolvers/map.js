9fcd560b2edd711bea32ad01c944cb97e0558f70
peteraflores <Japanboysushi@gmail.com>
2020-06-13 12:34:17 -0400

    #4 Get map uses where filter and create map is implemented

--- src/resolvers/map.js
+++ src/resolvers/map.js
@@ -1,19 +1,38 @@
 const { ApolloError } = require('apollo-server');
-const { CreateDocument, GetDocument } = require('../utils/database');
+const { CreateDocument, GetDocument, GetDocuments } = require('../utils/database');
 const { schema:Map } = require('../models/map');
 const { Where } = require('../utils/linqConstructor');
+const mongoose = require('mongoose');
 
 module.exports = {
     Query: {
-        getMap: ( _, { id, where } ) => {
-            if (id && where) {
+        getMap: ( _, { _id, where } ) => {
+            if (_id && where) {
                 throw new ApolloError("Cannot specify id and where clause!");
             } 
-            else if (id) {    
-                throw new ApolloError("Not implemented");
+            else if (_id) {    
+                return GetDocument(mongoose.model('Map', Map),{ _id: _id } )
+                .then( async (result) => {
+                    return [result]
+                })
+                .catch( (error) => {
+                    return {
+                        code: 400,
+                        message: error
+                    }
+                });        
             } 
             else if (where){
-                throw new ApolloError("Not implemented");
+                return GetDocuments(mongoose.model('Map', Map), Where(where) )
+                .then( async (result) => {
+                    return result
+                })
+                .catch( (error) => {
+                    return {
+                        code: 400,
+                        message: error
+                    }
+                }); 
             } 
             else {
                 throw new ApolloError("Must provide an id or a where clause!");
@@ -22,7 +41,7 @@ module.exports = {
     },
     Mutation: {
         createMap: ( _ , { map }) => {
-            return CreateDocument(Map, map)
+            return CreateDocument(mongoose.model('Map', Map), map)
                 .then( () => { 
                     return {
                         code: 200,
