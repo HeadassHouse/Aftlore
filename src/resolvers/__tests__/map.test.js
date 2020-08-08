5fd18ab68f84a4c956f6349a72c6ad73f9f061ec
River Marks <rmarks6767@gmail.com>
2020-08-08 00:22:02 -0400

    Made some various changes to the structure of the map query in preparations to add pagination

--- src/resolvers/__tests__/map.test.js
+++ src/resolvers/__tests__/map.test.js
@@ -1,45 +1,68 @@
 const { ApolloError, PubSub } = require('apollo-server');
 const { Query, Mutation, Subscription } = require('../map');
-const { Where } = require('../utils/linqConstructor');
+const { Where } = require('../utils/queryBuilder');
 const {
   CreateDocument, GetDocuments, GetDocument, UpdateDocument, DeleteDocument, DeleteDocuments,
 } = require('../utils/database');
 
 jest.mock('apollo-server');
-jest.mock('../utils/linqConstructor');
+jest.mock('../utils/queryBuilder');
 jest.mock('../utils/database');
 
 describe('map resolver', () => {
-  let map;
+  let maps;
 
   beforeEach(() => {
-    map = {
-      _id: '123',
-      imageLink: 'imageLink',
-      name: 'name',
-      tiles: [
-        {
-          _id: '1231',
-          x: 0,
-          y: 0,
-          width: 1,
-          height: 1,
-          type: 'PASSABLE',
-        },
-      ],
-      entities: [
-        {
-          type: 'CHARACTER',
-          associatedId: '456',
-        },
-      ],
-    };
-    CreateDocument.mockReturnValue(map);
-    DeleteDocument.mockReturnValue(map);
-    DeleteDocuments.mockReturnValue(map);
-    GetDocument.mockReturnValue(map);
-    GetDocuments.mockReturnValue([map]);
-    UpdateDocument.mockReturnValue(map);
+    maps = [
+      {
+        _id: '123',
+        imageLink: 'imageLink',
+        name: 'name',
+        tiles: [
+          {
+            _id: '1231',
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1,
+            type: 'PASSABLE',
+          },
+        ],
+        entities: [
+          {
+            type: 'CHARACTER',
+            associatedId: '456',
+          },
+        ],
+      },
+      {
+        _id: '456',
+        imageLink: 'imageLink',
+        name: 'name',
+        tiles: [
+          {
+            _id: '4561',
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1,
+            type: 'PASSABLE',
+          },
+        ],
+        entities: [
+          {
+            type: 'CHARACTER',
+            associatedId: '789',
+          },
+        ],
+      },
+    ];
+    CreateDocument.mockReturnValue(maps[0]);
+    DeleteDocument.mockReturnValue(maps[0]);
+    DeleteDocuments.mockReturnValue(maps[0]);
+    GetDocument.mockReturnValue(maps[0]);
+    GetDocuments.mockReturnValue(maps);
+    UpdateDocument.mockReturnValue(maps[0]);
   });
 
   afterEach(() => {
@@ -47,18 +70,18 @@ describe('map resolver', () => {
   });
 
   describe('query', () => {
-    describe('getMap', () => {
+    describe('map(s)', () => {
       it('should return a map when an _id is provided', async () => {
         const input = {
           _id: '123',
         };
 
-        const response = await Query.getMap(null, input);
+        const response = await Query.map(null, input);
 
-        expect(response).toEqual([map]);
+        expect(response).toEqual(maps[0]);
       });
 
-      it('should return map(s) when a where clause is provided', async () => {
+      it('should return maps when a where clause is provided', async () => {
         const input = {
           where: {
             and: [
@@ -75,49 +98,10 @@ describe('map resolver', () => {
           },
         };
 
-        const response = await Query.getMap(null, input);
+        const response = await Query.maps(null, input);
 
         expect(Where).toHaveBeenCalledWith(input.where);
-        expect(response).toEqual([map]);
-      });
-
-      it('should throw an error if _id and where provided', async () => {
-        const input = {
-          _id: '123',
-          where: {
-            and: [
-              {
-                or: [
-                  {
-                    property: 'prop',
-                    value: 'val',
-                    operation: 'EQUALS',
-                  },
-                ],
-              },
-            ],
-          },
-        };
-
-        try {
-          await Query.getMap(null, input);
-        } catch (_) {
-          expect(ApolloError).toHaveBeenCalledWith(
-            'Cannot specify id and where clause',
-            'ID_AND_WHERE',
-          );
-        }
-      });
-
-      it('should throw an error if no _id or where provided', async () => {
-        try {
-          await Query.getMap(null, {});
-        } catch (_) {
-          expect(ApolloError).toHaveBeenCalledWith(
-            'No id or where clause was provided',
-            'NO_ID_OR_WHERE',
-          );
-        }
+        expect(response).toEqual(maps);
       });
     });
   });
@@ -125,15 +109,15 @@ describe('map resolver', () => {
   describe('mutation', () => {
     describe('create map', () => {
       it('should return the map when created properly', async () => {
-        const response = await Mutation.createMap(null, { map });
+        const response = await Mutation.createMap(null, { map: maps[0] });
 
-        expect(response).toEqual(map);
+        expect(response).toEqual(maps[0]);
       });
 
       it('should throw an error when it could not create the map', async () => {
         CreateDocument.mockReturnValue(null);
         try {
-          await Mutation.createMap(null, { map });
+          await Mutation.createMap(null, { map: maps[0] });
         } catch (_) {
           expect(ApolloError).toHaveBeenCalledWith(
             'An error occurred while trying to create the map',
@@ -154,13 +138,13 @@ describe('map resolver', () => {
         };
         const response = await Mutation.editMap(null, info);
 
-        expect(response).toEqual(map);
+        expect(response).toEqual(maps[0]);
       });
 
       it('should throw an error when it could not edit the map', async () => {
         UpdateDocument.mockReturnValue(null);
         try {
-          await Mutation.editMap(null, { map });
+          await Mutation.editMap(null, { map: maps[0] });
         } catch (_) {
           expect(ApolloError).toHaveBeenCalledWith(
             'An error occurred while trying to edit the map',
@@ -178,7 +162,7 @@ describe('map resolver', () => {
 
         const response = await Mutation.deleteMap(null, input);
 
-        expect(response).toEqual(map);
+        expect(response).toEqual(maps[0]);
       });
 
       it('should return deleted map(s) when a where clause is provided', async () => {
@@ -201,7 +185,7 @@ describe('map resolver', () => {
         const response = await Mutation.deleteMap(null, input);
 
         expect(Where).toHaveBeenCalledWith(input.where);
-        expect(response).toEqual(map);
+        expect(response).toEqual(maps[0]);
       });
 
       it('should throw an error if it could not delete the given map with _id', async () => {
